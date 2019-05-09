eb4755abe728d4801de6200d77357cb53c257963
pulluruprashanth <pulluru29prashanth@gmail.com>
2019-05-09 18:05:19 +0530

    arithmrtic operaions

---
+++ arithops.sh
@@ -0,0 +1,15 @@
+#!/bin/bash      
+#title           :
+#description     :
+#author		 :prashanth 
+#date            :09052019
+#version         :1.0    
+#usage		     :
+
+expr 3 + 2
+expr 3 - 2
+expr 3 \* 2
+expr 10 / 2
+expr 20 % 3
+echo addition of 3 and 2 is : `expr 3 + 2`
+
